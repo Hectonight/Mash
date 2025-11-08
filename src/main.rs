2d597543783f484edf5b5c60b7d3c3150c553dd3
Hectonight <joshuaoring@yahoo.com>
2025-11-07 19:55:28 -0500

    No Longer need to hide jetbrains warning

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,13 @@ mod parser;
 mod types;
 mod interp;
 
-use crate::interp::interp;
+use interp::interp;
 use lexer::Tokens;
 
 use logos::Logos;
 use std::{env, fs};
 
 
-//noinspection RsUnresolvedPath
 fn main() {
     let filename = env::args().nth(1).expect("Expected file argument");
     let src = fs::read_to_string(&filename).expect("Failed to read file");
