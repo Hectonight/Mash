ed22a3a50dc6fa6850118ecd8b0c99e244f394be
Hectonight <joshuaoring@yahoo.com>
2025-11-18 17:27:34 -0500

    added some operators

--- src/compile_to_ir.rs
+++ src/compile_to_ir.rs
@@ -3,8 +3,8 @@ use crate::inter_rep::Imm::Int;
 use crate::inter_rep::IRInst::{Mov, Push, Ret, Section};
 use crate::inter_rep::Operand::{Imm, Reg};
 use crate::inter_rep::R64::{R15, RAX, RDI, RSP};
-use crate::constructors::{add, and, call, external, global, label, mov, pop, push, section, sub, xor};
-use crate::types::{Datum, Expr, TypedExpr, TypedStatement as Statement, TypedProgram as Program, Type};
+use crate::constructors::{add, and, call, external, global, label, mov, or, pop, push, section, sub, xor};
+use crate::types::{Datum, Expr, TypedExpr, TypedStatement as Statement, TypedProgram as Program, Type, Ops};
 
 
 pub fn compile_to_ir(program: &Program) -> AsmProg {
@@ -64,14 +64,84 @@ fn compile_expr(expr: &Expr) -> AsmProg {
     match expr {
         Expr::Datum(d) => compile_datum(d),
         Expr::Identifier(_) => todo!(),
-        Expr::Op(_) => todo!()
+        Expr::Op(op) => compile_op(op),
+    }
+}
+
+
+fn compile_op2(e1: &Expr, e2: &Expr) -> AsmProg {
+    let mut asm = compile_expr(e1);
+    asm.push(push(RAX));
+    asm.append(&mut compile_expr(e2));
+    asm
+}
+
+fn compile_op(op: &Ops) -> AsmProg {
+    match op {
+        Ops::Ternary(_, _, _) => todo!(),
+        Ops::Not(_) => todo!(),
+        Ops::BitNot(_) => todo!(),
+        Ops::Neg(_) => todo!(),
+        Ops::Pos(_) => todo!(),
+        Ops::Plus(e1, e2) => {
+            let mut asm = compile_op2(e1, e2);
+            asm.append(&mut vec![
+                pop(RDI),
+                add(RAX, RDI),
+            ]);
+            asm
+        },
+        Ops::Minus(e1, e2) => {
+            let mut asm = compile_op2(e1, e2);
+            asm.append(&mut vec![
+                pop(RDI),
+                sub(RDI, RAX),
+                mov(RAX, RDI)
+            ]);
+            asm
+        },
+        Ops::Mul(_, _) => todo!(),
+        Ops::Div(_, _) => todo!(),
+        Ops::Mod(_, _) => todo!(),
+        Ops::Eq(_, _) => todo!(),
+        Ops::Neq(_, _) => todo!(),
+        Ops::Lt(_, _) => todo!(),
+        Ops::Leq(_, _) => todo!(),
+        Ops::Gt(_, _) => todo!(),
+        Ops::Geq(_, _) => todo!(),
+        Ops::BitAnd(e1, e2) | Ops::And(e1, e2) => {
+            let mut asm = compile_op2(e1, e2);
+            asm.append(&mut vec![
+                pop(RDI),
+                and(RAX, RDI),
+            ]);
+            asm
+        },
+        Ops::BitOr(e1, e2) | Ops::Or(e1, e2) => {
+            let mut asm = compile_op2(e1, e2);
+            asm.append(&mut vec![
+                pop(RDI),
+                or(RAX, RDI),
+            ]);
+            asm
+        },
+        Ops::BitXor(e1, e2) => {
+            let mut asm = compile_op2(e1, e2);
+            asm.append(&mut vec![
+                pop(RDI),
+                xor(RAX, RDI),
+            ]);
+            asm
+        },
+        Ops::BitShiftLeft(_, _) => todo!(),
+        Ops::BitShiftRight(_, _) => todo!(),
     }
 }
 
 fn compile_datum(datum: &Datum) -> AsmProg {
     vec![match datum {
         Datum::Int(i) => mov(RAX, *i),
-        Datum::Bool(b) => todo!(),
+        Datum::Bool(b) => mov(RAX, 1),
         Datum::Null => todo!()
     }]
 }
